1b3ee3d2c52b07144db4513dae266eb9ba72126a
Shiro-cha <noum.rzdr@gmail.com>
2022-11-06 02:44:29 +0300

    Message by stp

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -50,7 +50,7 @@ module.exports = {
                     if(fileContain.addressServer && fileContain.passwordServer && fileContain.userServer){
                       const ssh = new Client()
                       const arrayPath=urlObject.pathname.split("/")
-                      const targetPath = option.target || arrayPath[arrayPath.length-1]
+                      const targetPath = option.target || arrayPath[arrayPath.length-1].split(".")[this.length-1]
                       console.log(targetPath);
                       // ssh.on("ready",()=>{
                       //     console.log("[Success: Server]");
