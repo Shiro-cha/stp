d91e5429b39ca8801c8524b97b85605d753a8bb0
Shiro-cha <noum.rzdr@gmail.com>
2022-11-05 19:15:19 +0300

    File configuration first verification

--- controllers/config.controller.js
+++ controllers/config.controller.js
@@ -1,9 +1,34 @@
 const bzr = require("../bazar.function")
+const fs = require("fs")
+const {join} = require("path")
+const {userInfo} = require("os")
+const fileConfig = join(userInfo().homedir,".stp_rc")
 
 module.exports={
   //set configuration
-  setConfiguration:options=>{
-    console.log(options)
+  setConfiguration: options=>{
+    //verify if the file configuration exist
+    fs.stat(fileConfig,(err,stats)=>{
+      //verify if an error occure
+      if(err){
+        //error linked in file doesn't exist
+
+        if(err.errno===-2 && err.code==="ENOENT" && err.path === fileConfig){
+          //create the file because it doesn't exist
+          fs.append(fileConfig,"{}",err=>{
+            if(err){
+              console.log(err.message)
+            }else{
+              //next step pass to the configuration function
+              
+            }
+          })
+        }
+      }else{
+        console.log(stats)
+      }
+    })
+
   },
   resetConfiguration:options=>{
     console.log(options)
