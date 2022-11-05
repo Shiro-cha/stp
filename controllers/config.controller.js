6e02caa5e37ebaeb18f017b2d10aed877e3ed56f
Shiro-cha <noum.rzdr@gmail.com>
2022-11-05 18:40:11 +0300

    Configuration de l'architecture des fichiers

---
+++ controllers/config.controller.js
@@ -0,0 +1,11 @@
+const bzr = require("../bazar.function")
+
+module.exports={
+  //set configuration
+  setConfiguration:options=>{
+    console.log(options)
+  },
+  resetConfiguration:options=>{
+    console.log(options)
+  }
+}
